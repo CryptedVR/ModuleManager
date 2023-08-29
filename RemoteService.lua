a2362d1273c0b26826e0d319df129ae29b030ad4
Crypted <41970118+CryptedVR@users.noreply.github.com>
2023-08-29 18:53:01 +0200

    Update RemoteService.lua

--- RemoteService.lua
+++ RemoteService.lua
@@ -3,34 +3,28 @@
 --- SERVICES
 local RPS = game:GetService("ReplicatedStorage");
 
---- VARIABLES
---/ Objects
+--- CONSTANTS
 local Remotes = RPS:WaitForChild("Remotes");
-
---/ Module
 local M = {};
 
-local M_Funcs = {};
-M_Funcs.__index = M_Funcs;
-
 --- FUNCTIONS
---/ Module Functions
-function M.Hook(RemoteName :string, Timeout :number?) -- Main function, hooks up the functions as long as a remote called "RemoteName" is within a folder called "Remotes" in ReplicatedStorage
-	local New = {};
-	setmetatable(New, M_Funcs);
+local _M = {};
+_M.__index = _M;
+
+function M.Hook(RemoteName :string, Timeout :number?)
+	local New = setmetatable({}, _M);
 	
 	New.Remote = Remotes:WaitForChild(RemoteName, Timeout);
 	
 	return New;
 end;
 
---/ OOP Functions
-function M_Funcs:Send(...) :nil -- Exists for simplicity
+function _M:Send(...) :nil
 	self.Remote:FireServer(...);
 	return;
 end;
 
-function M_Funcs:Get(...) :any -- Sends a request to Server, yields until response
+function _M:Get(...) :any
 	self.Remote:FireServer(...);
 	return self.Remote.OnClientEvent:Wait();
 end;
