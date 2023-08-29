3f24a35dd73958e14f15f5f663481ea43b6f12d4
Crypted <41970118+CryptedVR@users.noreply.github.com>
2023-08-29 18:50:31 +0200

    Update RemoteService.lua

--- RemoteService.lua
+++ RemoteService.lua
@@ -1,68 +1,76 @@
 -- Crypted#9928 - https://github.com/CryptedVR/ModuleManager
 
+--- CORE
+function NewInstance(ClassName :string, Parent :Instance, Properties :{[string] :any}) :Instance
+	local Object :Instance = Instance.new(ClassName, Parent);
+	
+	for Property :string, Value :any in Properties do
+		Object[Property] = Value;
+	end;
+	
+	return Object;
+end;
+
+function VerifyDirectory(Root :Instance, Path :string, Seperator :string?) :Instance
+	local Current :Instance = Root;
+	
+	for _,Index :string in string.split(Path, Seperator or ".") do
+		Current = Current:WaitForChild(Index) or NewInstance("Folder", Current, {
+			Name = Index,
+		});
+	end;
+	
+	return Current;
+end;
+
 --- SERVICES
 local P = game:GetService("Players");
 local RPS = game:GetService("ReplicatedStorage");
 
---- VARIABLES
---/ Objects
-local Remotes = RPS:FindFirstChild("Remotes") or Instance.new("Folder", RPS);
-Remotes.Name = "Remotes";
-
---/ Module
+--- CONSTANTS
+local Remotes = VerifyDirectory(RPS, "Remotes");
 local M = {};
 
+--- SETTINGS
 M.CreateMissingRemotes = false;
-
-local M_Funcs = {};
-M_Funcs.__index = M_Funcs;
+M.SendDefaultsToAll = true;
 
 --- FUNCTIONS
---/ Module Functions
-function M.Hook(RemoteName :string, Timeout :number?) -- Main function, hooks up the functions as long as a remote called "RemoteName" is within a folder called "Remotes" in ReplicatedStorage
-	local New = {};
-	setmetatable(New, M_Funcs);
+local _M = {};
+_M.__index = _M;
+
+function M.Hook(RemoteName :string, Timeout :number?)
+	local New = setmetatable({}, _M);
 	
 	if M.CreateMissingRemotes and Remotes:FindFirstChild(RemoteName) == nil then
 		New.Remote = Instance.new("RemoteEvent", Remotes);
 		New.Remote.Name = RemoteName;
 	else
-		Remotes:WaitForChild(RemoteName, Timeout);
+		New.Remote = Remotes:WaitForChild(RemoteName, Timeout);
 	end;
 	
 	return New;
 end;
 
---/ OOP Functions
-function M_Funcs:Send(Targets :{Player}?, ...) :nil -- Exists for simplicity, and somewhat QOL
-	for _,Target :Player in (Targets or P:GetPlayers()) do
-		self.Remote:FireClient(Target, ...);
+function _M:Send(Targets :(Player | {Player})?, ...) :nil
+	if typeof(Targets) == "table" then
+		for _,Target :Player in (Targets or (if (M.SendDefaultsToAll) then P:GetPlayers() else {})) do
+			self.Remote:FireClient(Target, ...);
+		end;
+	else
+		self.Remote:FireClient(Targets, ...);
 	end;
 	
 	return;
 end;
 
-function M_Funcs:HookReturning(ReceiveFunction :(Player, ...any) -> (boolean, string)) :nil -- Waits for signals from clients, triggers function and sends whatever the function returns to the client
+function _M:HookReturning(ReceiveFunction :(Player, ...any) -> (...any)) :nil -- Waits for signals from clients, triggers function and sends whatever the function returns to the client
 	self.Remote.OnServerEvent:Connect(function(Client :Player, ...)
 		self.Remote:FireClient(Client, ReceiveFunction(Client, ...));
 	end);
 	
 	return;
 end;
-
---[[ Easy HookReturning usage
-
-local ActionTypes :([string] :(Player, ...any) -> (boolean, string)) = {
-	["Test"] = function(Client :Player, Argument :any)
-		return "Some response " .. Argument;
-	end;
-};
-
-Remote:HookReturning(function(Client :Player, Action :string, ...)
-	return pcall(ActionTypes[Action], ...);
-end);
-
-]]
 
 --- MODULE RETURN
 return M;
